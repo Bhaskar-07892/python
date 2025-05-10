5ddfa748527fcda87fbf64bdb2c261c332428535
Bhaskar-07892 <bhaskarpathak5607@gmail.com>
2025-05-10 18:51:56 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# ---------Tuple ( ) ---------
-
-# A tuple is an immutable data type in Python, meaning its elements cannot be changed after creation.
-
-fruit_tuple = ("apple" , "banana", "cherry")
-print(fruit_tuple)
